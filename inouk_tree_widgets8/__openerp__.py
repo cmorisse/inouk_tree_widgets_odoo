7e0a7fe2cb5d859fd1297cac8512fca8bb66e03c
Cyril MORISSE <cmorisse@boxes3.net>
2015-03-02 06:48:02 +0100

    FIX: .gitignore to manage fancytree dist folder

--- inouk_tree_widgets8/__openerp__.py
+++ inouk_tree_widgets8/__openerp__.py
@@ -8,12 +8,14 @@ Inouk Tree
 Advanced tree selector.
 """,
     'version': '0.1',
-    'depends': ['base'],
+    'depends': ['web'],
     'data': [
         # This is how we load ou javascript
         'views/jsloader.xml'
     ],
-    'qweb': ['static/src/xml/*.xml'],
+    'qweb': [
+        'static/src/xml/*.xml'
+    ],
     'installable': True,
     'auto_install': False
 }
